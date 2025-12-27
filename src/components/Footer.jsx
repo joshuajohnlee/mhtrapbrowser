c055352ed795b09ec0089338975746ba16f31dbe
Joshua Lee <joshuajohnlee@me.com>
2025-12-27 20:47:26 +0000

    Footer amended

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function PageFooter() {
     return (
         <>
         <div className="app-footer">
-            <p id="footer-credit">Made by JoystickJoshy</p>
+            <p id="footer-credit">Made by Joshua Lee</p>
             <p id="footer-links"><a href="https://github.com/joshuajohnlee/mhtrapbrowser" aria-label="GitHub Link"><FontAwesomeIcon aria-hidden={true} icon={faGithub} /> GitHub</a></p>
         </div>
         </>
